7f0f2edb5914f6f40ef5adce70c2a3ea78a9015f
Rajan <rajangundigara@gmail.com>
2021-05-24 00:42:50 +0530

    Started With PayPal Payments & Some Bug Fixes

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,5 +1,13 @@
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 const { v4: uuidv4 } = require("uuid");
+const braintree = require("braintree");
+
+const gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.PP_MERCH_ID,
+  publicKey: process.env.PP_PUB_KEY,
+  privateKey: process.env.PP_PRIVATE_KEY,
+});
 
 exports.payViaStripe = (req, res) => {
   const { products, token } = req.body;
@@ -26,8 +34,8 @@ exports.payViaStripe = (req, res) => {
       stripe.charges
         .create(
           {
-            amount: total * 100,
-            currency: "usd",
+            amount: total * 100, //multiply with 100 as it deals in cents/paise
+            currency: "INR",
             customer: customer.id, // set the customer id
             receipt_email: token.email,
             description: "Purchase Made On TeeStore",
@@ -52,4 +60,36 @@ exports.payViaStripe = (req, res) => {
         .catch((err) => console.log(err));
     })
     .catch((err) => console.log("Errrrrr : ", err));
+};
+
+exports.payViaPaypal = (req, res) => {
+  let nonceFromTheClient = req.body.paymentMethodNonce;
+  let amountFromTheClient = req.body.amount;
+
+  gateway.transaction.sale(
+    {
+      amount: amountFromTheClient,
+      paymentMethodNonce: nonceFromTheClient,
+      options: {
+        submitForSettlement: true,
+      },
+    },
+    (err, result) => {
+      if (err) {
+        return res.status(500).json(err);
+      } else {
+        return res.json(result);
+      }
+    }
+  );
+};
+
+exports.getPaypalToken = (req, res) => {
+  gateway.clientToken.generate({}, (err, response) => {
+    if (err) {
+      return res.status(500).json(err);
+    } else {
+      return res.json(response);
+    }
+  });
 };
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,18 +3,34 @@ const router = express.Router();
 
 const { isSignedIn, isAuthenticated } = require("../controllers/auth");
 
-const { payViaStripe } = require("../controllers/payment");
+const {
+  payViaStripe,
+  payViaPaypal,
+  getPaypalToken,
+} = require("../controllers/payment");
 const { getUserById } = require("../controllers/user");
 
-//router.param("userId", getUserById);
+router.param("userId", getUserById);
 
-// router.post(
-//   "/stripePayment/:userId",
-//   isSignedIn,
-//   isAuthenticated,
-//   payViaStripe
-// );
+router.post(
+  "/stripePayment/:userId",
+  isSignedIn,
+  isAuthenticated,
+  payViaStripe
+);
 
-router.post("/stripePayment", payViaStripe);
+router.get(
+  "/payPalPayment/getToken/:userId",
+  isSignedIn,
+  isAuthenticated,
+  getPaypalToken
+);
+
+router.post(
+  "/payPalPayment/:userId",
+  isSignedIn,
+  isAuthenticated,
+  payViaPaypal
+);
 
 module.exports = router;
